584fd8f494e8700b7c257727eb5ccaba9abb7302
wjxiu <wujinxiu2002@gmail.com>
2024-01-06 20:31:30 +0800

    init

---
+++ src/main/java/com/github/wjxiu/Main.java
@@ -0,0 +1,11 @@
+package com.github.wjxiu;
+
+/**
+ * @author xiu
+ * @create 2024-01-06 20:30
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
